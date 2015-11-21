35535561a60920f2b4ab0ef956295ff0767150cb
Thomas Uster <thomasuster@gmail.com>
2015-11-21 12:08:31 -0800

    Potential fix for https://github.com/haxenme/nme/issues/275.

--- templates/android/PROJ/src/org/haxe/nme/GameActivity.java
+++ templates/android/PROJ/src/org/haxe/nme/GameActivity.java
@@ -786,7 +786,6 @@ implements SensorEventListener
       mView.sendActivity(NME.DESTROY);
       if (mVideoView!=null)
          mVideoView.stopPlayback();
-      activity = null;
       super.onDestroy();
    }
 
