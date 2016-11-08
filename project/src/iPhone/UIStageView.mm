be093d7e1ab398f02feb6ccf679ee52ea621dcea
Thomas Uster <thomasuster@gmail.com>
2016-11-08 15:07:43 -0800

    Fix to allow rotate screen, also fixes facebook login and facebook send

--- project/src/iPhone/UIStageView.mm
+++ project/src/iPhone/UIStageView.mm
@@ -1981,7 +1981,7 @@ bool nmeIsMain = true;
 
 - (NSUInteger)supportedInterfaceOrientations
 {
-   return UIInterfaceOrientationLandscapeLeftMask;
+   return UIInterfaceOrientationLandscapeLeftMask | UIInterfaceOrientationLandscapeRightMask;
 
    int mask = 1;
    bool isOverridden = false;
