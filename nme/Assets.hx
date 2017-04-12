17b9cfad02798692032c27ff49c0dbc8e61b84f9
Thomas Uster <thomasuster@gmail.com>
2017-04-12 09:05:25 -0700

    Allow library unloads

--- nme/Assets.hx
+++ nme/Assets.hx
@@ -589,7 +589,11 @@ class Assets
       } );
    }
 
-
+    public static function unloadLibrary(inLibName:String)
+    {
+        loadedLibraries.remove(inLibName);
+    }
+    
    public static function getLoadedLibrary(inLibName:String) : AssetLib
    {
       if (!loadedLibraries.exists(inLibName))
