20bd5c027425b5ba35f45796f66577e0adaab017
Thomas Uster <thomasuster@gmail.com>
2017-12-09 15:42:15 -0800

    dont save crap

--- tools/nme/src/helpers/ImageHelper.hx
+++ tools/nme/src/helpers/ImageHelper.hx
@@ -47,7 +47,7 @@ class ImageHelper
       var data = new BitmapData(width, height, true, backgroundColor);
       data.draw(bitmap,matrix);
 
-      sys.io.File.saveBytes("crap.jpg", data.encode("jpg",1) );
+      //sys.io.File.saveBytes("crap.jpg", data.encode("jpg",1) );
 
       return data;
    }
